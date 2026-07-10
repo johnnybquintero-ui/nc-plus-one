1c97a06ae7c57a45b920b15f7e07a5f7a9831df1
johnnybquintero-ui <johnnybquintero@gmail.com>
2026-07-10 14:27:52 +0100

    Add Terraform configuration for RDS PostgreSQL

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,13 @@ output "instance_public_ip" {
   description = "Public IP address of the NC Plus One EC2 instance"
   value       = aws_instance.nc_plus_one.public_ip
 }
+
+output "rds_endpoint" {
+  description = "RDS PostgreSQL hostname"
+  value       = aws_db_instance.nc_plus_one.address
+}
+
+output "rds_port" {
+  description = "RDS PostgreSQL port"
+  value       = aws_db_instance.nc_plus_one.port
+}
