ace3698a95c71d09946a9ea491b76e9db55fe2d2
johnnybquintero-ui <johnnybquintero@gmail.com>
2026-07-10 12:00:47 +0100

    feat: initialise Terraform project with remote S3 backend

---
+++ terraform/provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+      Project = "nc-plus-one"
+      Team = "Data Engineering"
+      DeployedFrom = "Terraform"
+      Repository = "nc-plus-one"
+      Environment = "dev"
+    }
+  }
+}
